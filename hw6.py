02e0ff1b9e37f04ae300bacd0f9514824c95f527
hihhi11 <jiwon051130@anver.com>
2024-04-19 22:26:38 +0900

    add hw6.py

---
+++ hw6.py
@@ -0,0 +1,9 @@
+for i in range(1,10):
+    for u in range(2,6):
+        print(f'{u} * {i} = {u*i:2d}', end ='\t')
+    print()
+print()
+for i in range(1,10):
+    for u in range(6,10):
+        print(f'{u} * {i} = {u*i:2d}', end ='\t')
+    print()
